d08dbbc148d6e42c5a90649976af0afac0557919
parasmer <138353860+parasmer@users.noreply.github.com>
2026-01-23 18:36:49 +0530

    Add overflow-hidden class to HomePage div

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage=({setisLoggedIn})=>{
 return(
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-screen overflow-hidden items-center justify-center">
     <h1 className="text-white font-extrabold"> Welcome to RouteShield</h1>
     </div>
 )
